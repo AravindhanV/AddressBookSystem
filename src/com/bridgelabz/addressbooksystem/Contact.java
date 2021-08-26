f02f622f8e458312da4e12963f10ab22aef4dbd2
Aravindhan V <aravindhan.v@perfios.com>
2021-08-26 17:15:24 +0530

    REFACTOR: Made Contact variables non-final

--- src/com/bridgelabz/addressbooksystem/Contact.java
+++ src/com/bridgelabz/addressbooksystem/Contact.java
@@ -1,19 +1,19 @@
 package com.bridgelabz.addressbooksystem;
 
 public class Contact {
-	private final String firstName;
-	private final String lastName;
-	private final String city;
-	private final String state;
-	private final String zip;
-	private final String phoneNumber;
-	private final String email;
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
 
 	public Contact(String firstName, String lastName, String city, String state, String zip, String phoneNumber,
 			String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.city = city;
+		this.setCity(city);
 		this.state = state;
 		this.zip = zip;
 		this.phoneNumber = phoneNumber;
@@ -24,27 +24,55 @@ public class Contact {
 		return city;
 	}
 
-	public String getLastName() {
-		return lastName;
+	public void setCity(String city) {
+		this.city = city;
 	}
 
 	public String getFirstName() {
 		return firstName;
 	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	public String getState() {
 		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
 	}
 
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 	public String getEmail() {
 		return email;
 	}
 
-	public String getZip() {
-		return zip;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
