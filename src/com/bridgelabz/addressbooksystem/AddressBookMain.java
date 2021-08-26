62737c437e56d3ccd5aa650cbb931e42257c4b92
Aravindhan V <aravindhan.v@perfios.com>
2021-08-26 17:09:26 +0530

    ADDED: Add Contact to address book feature

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.addressbooksystem;
 
+import com.bridgelabz.addressbooksystem.*;
+
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact();
 	}
 }
