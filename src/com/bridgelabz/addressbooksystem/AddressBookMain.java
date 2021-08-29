d5ff1d47aafb2a2c75fb1559b9162d8c4e7a8e43
Aravindhan V <aravindhan.v@perfios.com>
2021-08-29 14:59:45 +0530

    REFACTOR: Exported user input to functions from main

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,15 +1,27 @@
 package com.bridgelabz.addressbooksystem;
 
-import com.bridgelabz.addressbooksystem.*;
 import java.util.*;
 
 public class AddressBookMain {
 	private AddressBook[] addressBooks;
 	private int noOfAddressBooks;
+	private static Scanner scanner = new Scanner(System.in);
 
 	public AddressBookMain() {
 		this.addressBooks = new AddressBook[5];
 		this.noOfAddressBooks = 0;
+	}
+	
+	public static int readBookChoice() {
+		System.out.println("Enter ID of address book (-1 to exit):");
+		int bookChoice = Integer.parseInt(scanner.nextLine());
+		return bookChoice;
+	}
+	
+	public static int readActionChoice() {
+		System.out.println("Enter 1. Add,2. Edit, 3. Delete, 4. Exit");
+		int actionChoice = Integer.parseInt(scanner.nextLine());
+		return actionChoice;
 	}
 
 	public static void main(String[] args) {
@@ -17,16 +29,13 @@ public class AddressBookMain {
 		AddressBookMain addressBookMain = new AddressBookMain();
 		addressBookMain.addAddressBook();
 		addressBookMain.addAddressBook();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter ID of address book (-1 to exit):");
-		int bookChoice = Integer.parseInt(scanner.nextLine());
+		
+		int bookChoice = readBookChoice();
 		if (bookChoice == -1) {
 			return;
 		}
 		while (true) {
-			System.out.println("Enter 1. Add,2. Edit, 3. Delete, 4. Exit");
-			int actionChoice = Integer.parseInt(scanner.nextLine());
-			switch (actionChoice) {
+			switch (readActionChoice()) {
 			case 1:
 				addressBookMain.addressBooks[bookChoice].addContact();
 				break;
