8504c794f1a838b2d29efba0d66da95f4746f440
Aravindhan V <aravindhan.v@perfios.com>
2021-09-22 18:44:05 +0530

    REMOVED: logic that read input and now only shows menu

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -4,125 +4,91 @@ import java.io.IOException;
 import java.util.*;
 
 public class AddressBookMain {
-	private HashMap<String, AddressBook> addressBooks;
-	private static Scanner scanner = new Scanner(System.in);
-
-	public AddressBookMain() {
-		this.addressBooks = new HashMap<>();
-	}
-
-	public static String readBookChoice() {
-		System.out.println("Enter name of address book (-1 to exit):");
-		String bookChoice = scanner.nextLine();
-		return bookChoice;
-	}
-
-	public static String readCity() {
-		System.out.println("Enter City:");
-		String city = scanner.nextLine();
-		return city;
+static Scanner sc=new Scanner(System.in);
+	
+	public static void main(String[] args) throws CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, IOException {
+		System.out.println("Welcome to Address Book");
+		getMenu();
 	}
 	
-	public static String readState() {
-		System.out.println("Enter State:");
-		String state = scanner.nextLine();
-		return state;
-	}
-
-	public static int readActionChoice() {
-		System.out.println(
-				"Enter 1. Add,2. Edit, 3. Delete, 4. Search by City across books, 5. Search by state across books, 6. Contact summary,7. Sorted Contacts,8. Write book to file,9. Read/Write contacts with CSV 10. Exit");
-		int actionChoice = Integer.parseInt(scanner.nextLine());
-		return actionChoice;
-	}
-	
-	public void searchAcrossByCity(String city) {
-		for(AddressBook addressBook : addressBooks.values()) {
-			addressBook.findContactInCity(city);
-		}
-	}
-	
-	public void searchAcrossByState(String state) {
-		for(AddressBook addressBook : addressBooks.values()) {
-			addressBook.findContactInState(state);
-		}
-	}
-	
-	public void printTotalSummary() {
-		for(AddressBook addressBook : addressBooks.values()) {
-			addressBook.printSummary();
-			System.out.println("--------------------------------");
-		}
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program");
-		AddressBookMain addressBookMain = new AddressBookMain();
-		addressBookMain.addAddressBook("book1");
-		addressBookMain.addAddressBook("book2");
-
-		String bookChoice = readBookChoice();
-		if (bookChoice == "" || addressBookMain.addressBooks.get(bookChoice) == null) {
-			return;
-		}
-		while (true) {
-			switch (readActionChoice()) {
-			case 1:
-				addressBookMain.addressBooks.get(bookChoice).addContact();
-				break;
-
-			case 2:
-				addressBookMain.addressBooks.get(bookChoice).editContact();
-				break;
-
-			case 3:
-				addressBookMain.addressBooks.get(bookChoice).deleteContact();
-				break;
-
-			case 4:
-				String city = readCity();
-				addressBookMain.searchAcrossByCity(city);
-				break;
-
-			case 5:
-				String state = readState();
-				addressBookMain.searchAcrossByState(state);
-				break;
-				
-			case 6:
-				addressBookMain.printTotalSummary();
-				break;
-				
-			case 7: addressBookMain.addressBooks.get(bookChoice).getSortedContacts();
-			break;
+	public static void getMenu() throws CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, IOException {
+		AddressBookArray addressBookArray=new AddressBookArray();
+		
+		
+		while(true) {
+			System.out.println("1.Add new Address Book\n2.Edit Existing\n3.Search in City\n4.Search in State\n5.Display Contacts in city\n6.Display Contacts in state\n7.Display Number of Contacts in Cities\n8.Display number of Contacts in States\n9.Write to File\n10.Read From File\n11.Exit");
+			int option=sc.nextInt();
+			contactMenu:
+			switch(option) {
 			
-			case 8: addressBookMain.writeAddressBookToFile(bookChoice);
-			break;
-			
-			case 9: addressBookMain.readAndWriteCSV(bookChoice);
-			break;
+				case 1: System.out.println("Enter name of Address Book");
+						String name=sc.next();
+						addressBookArray.addAddressBook(name);
+						break;
+				
+				case 2:	System.out.println("Enter name of address book to be edited");
+						String addressBookName=sc.next();
+						AddressBook addressBookToBeEdited=addressBookArray.getAddressBook(addressBookName);
+						while(true) {
+							System.out.println("1.Add Contact\n2.Edit Contact\n3.Delete Contact\n4.Exit\nSelect option");
+							int choice=sc.nextInt();
+							switch(choice) {
 
-			case 10:
-				System.exit(0);
-				break;
+							case 1: Contact contactDetails=addressBookToBeEdited.createContact();
+									addressBookToBeEdited.addContact(contactDetails);
+									break;
+							case 2: addressBookToBeEdited.editContact();
+									break;
+							case 3: addressBookToBeEdited.deleteContact();
+									break;
+							case 4: break contactMenu;
+				
+			
+							}
+						}
+						
+				case 3: System.out.println("Enter name of person");
+						String searchNameInCity=sc.next();
+						System.out.println("Enter name of city");
+						String cityName=sc.next();
+						addressBookArray.searchAcrossCity(searchNameInCity,cityName);
+						break;
+				
+				case 4: System.out.println("Enter name of person");
+						String searchNameInState=sc.next();
+						System.out.println("Enter name of state");
+						String stateName=sc.next();
+						addressBookArray.searchAcrossState(searchNameInState,stateName);
+						break;
+				
+				case 5: addressBookArray.viewContactsInCity();
+						break;
+				
+				case 6: addressBookArray.viewContactsInState();
+						break;
+						
+				case 7: addressBookArray.numberOfContactsInCity();
+						break;
+				
+				case 8: addressBookArray.numberOfContactsInState();
+						break;
+				
+				case 9: System.out.println("Enter name of AddressBook to be written to file");
+						String nameOfAddressBook=sc.next();
+						AddressBook addressBook=addressBookArray.getAddressBook(nameOfAddressBook);
+						addressBook.writeData(nameOfAddressBook+".json",IOService.JSON_IO);
+						break;
+				
+				case 10: System.out.println("Enter name of AddressBook to be read from file");
+						String nameOfAddressBookRead=sc.next();
+						System.out.println(addressBookArray.getAddressBook(nameOfAddressBookRead));
+						AddressBook.readData(nameOfAddressBookRead+".json", IOService.JSON_IO);
+						break;
+				
+				case 11: System.exit(0);
+		
+			
 			}
 		}
-	}
-	
-	public void readAndWriteCSV(String bookName) {
-		try {
-			addressBooks.get(bookName).readFromCsv("read.csv");
-			addressBooks.get(bookName).writeToCsv("write.csv");
-		} catch (Exception e) {
-		}
-	}
-	
-	public void writeAddressBookToFile(String bookName) {
-		addressBooks.get(bookName).writeToFile(bookName+".txt");
-	}
-
-	public void addAddressBook(String bookName) {
-		AddressBook addressBook = new AddressBook();
-		addressBooks.put(bookName, addressBook);
 	}
 }
