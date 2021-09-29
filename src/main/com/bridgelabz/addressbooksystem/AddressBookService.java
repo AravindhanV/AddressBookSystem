3f3bbacd8cb48be922a3d873a7b8734f444834b0
Aravindhan V <aravindhan.v@perfios.com>
2021-09-29 07:52:53 +0530

    ADDED: getConnection() to addressBookService

--- src/main/com/bridgelabz/addressbooksystem/AddressBookService.java
+++ src/main/com/bridgelabz/addressbooksystem/AddressBookService.java
@@ -1,5 +1,18 @@
 package com.bridgelabz.addressbooksystem;
 
-public class AddressBookService {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class AddressBookService {
+	private Connection getConnection() throws SQLException {
+		String jdbcURL="jdbc:mysql://localhost:3306/addressBookService?useSSL=false";
+		String userName="user1";
+		String password="pass";
+		Connection connection;
+		System.out.println("Connecting to database"+ jdbcURL);
+		connection=DriverManager.getConnection(jdbcURL,userName,password);
+		System.out.println("Connection is successfull"+ connection);
+		return connection;
+	}
 }
